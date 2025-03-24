ba01da2d8677604d7bd3863df52c47a3496a076b
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2025-03-24 23:03:15 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 9.03 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.py
+++ 프로그래머스/0/120829. 각도기/각도기.py
@@ -1,3 +1,5 @@
 def solution(angle):
     answer = 1 if 0<angle<90 else 2 if angle == 90 else 3 if 90<angle<180 else 4
+    
+    # answer = (angle // 90) * 2 + (angle % 90 > 0) * 1
     return answer
