5955e32e4228620ab1906fe70004f247c49f28af
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2025-04-12 23:53:48 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,12 +1,2 @@
 def solution(a, b):
-    str1 = str(a) + str(b)
-    str2 = str(b) + str(a)
-
-    if (str1 < str2):
-        answer = str2
-    elif (str2 < str1):
-        answer = str1
-    else:
-        answer = str1
-
-    return int(answer)
+    return int(max(f'{a}{b}', f'{b}{a}'))
