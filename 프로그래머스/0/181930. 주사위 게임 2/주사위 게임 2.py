53cb5edc3cc4206f3398dc52b526d99f692624b7
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2025-05-06 21:58:34 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 9.33 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -0,0 +1,8 @@
+def solution(a, b, c):
+    answer = 0
+    if len({a, b, c}) == 3 :
+        return a + b + c
+    elif len({a, b, c}) ==  2 :
+        return (a + b + c) * (a**2 + b**2 + c**2)
+    else :
+        return (a + b + c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3)
