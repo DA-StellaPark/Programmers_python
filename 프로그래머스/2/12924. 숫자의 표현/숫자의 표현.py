5b22eed0af2a1f80b5d36adf8add1a010d5005d0
benny <107995643+DAbenny-park@users.noreply.github.com>
2025-08-15 00:10:39 +0900

    [level 2] Title: 숫자의 표현, Time: 6.30 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,13 +1,12 @@
 def solution(n):
-    answer = 0
-    
-    for i in range(1, n+1):
-        sum_val = 0
-        for j in range(i, n+1):
-            sum_val += j
-            if sum_val == n:
-                answer += 1
-            elif sum_val > n:
+    count = 0                      
+    for i in range(1, n+1):         # 15=15
+        suml = 0                     
+        for j in range(i, n+1):     # i값을 시작으로 반복문 실행
+            suml += j               # i값부터 계속해서 값을 더해준다
+            if suml == n:           # 더한 값이(sumN)이 n과 같다면 count +1, break
+                count += 1          
                 break
-                
-    return answer
+            if suml > n:            # 더한 값(sumN)이 n보다 크다면 계산할 필요가 없음
+                break
+    return count
