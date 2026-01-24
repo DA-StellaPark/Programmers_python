9a156fa4f0beba092d7efdf1cf65ec20d70f3772
Stella <107995643+DA-StellaPark@users.noreply.github.com>
2026-01-24 13:56:25 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.py
+++ 프로그래머스/0/120829. 각도기/각도기.py
@@ -1,5 +1,4 @@
 def solution(angle):
-    answer = 1 if 0<angle<90 else 2 if angle == 90 else 3 if 90<angle<180 else 4
-    
-    # answer = (angle // 90) * 2 + (angle % 90 > 0) * 1
+    # answer = 1 if 0<angle<90 else 2 if angle == 90 else 3 if 90<angle<180 else 4
+    answer = (angle // 90) * 2 + (angle % 90 > 0) * 1
     return answer
