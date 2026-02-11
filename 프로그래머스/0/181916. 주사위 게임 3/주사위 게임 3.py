386cb57fbeae3efa9a8f7babb0039455dc023bb7
Stella <107995643+DA-StellaPark@users.noreply.github.com>
2026-02-11 19:47:24 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.03 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
@@ -1,19 +1,14 @@
 def solution(a, b, c, d):
-    check = set([a,b,c,d])
-    list_dice = [a,b,c,d]
-    if len(check)==1:
-        return 1111*list(check)[0] 
-    else : 
-        for n in check :
-            if list_dice.count(n)==2:
-                if len(check)==2: # 주사위 2개씩 같은 값인 경우
-                    check = list(check)
-                    return (check[0]+check[1]) * abs(check[0]-check[1])    
-                else : # 주사위 2개 값만 같고 나머지는 틀린 경우 
-                    check.remove(n) # 중복된 값 제거
-                    check = list(check) # list 변환
-                    return check[0]*check[1]
-            elif list_dice.count(n)==3 :
-                check.remove(n) # 중복된 값 제거 
-                return (10*n+list(check)[0])**2
-    return min(list(check))
+    l = [a,b,c,d]
+    c = [l.count(x) for x in l]
+    if max(c) == 4:
+        return 1111*a
+    elif max(c) == 3:
+        return (10*l[c.index(3)]+l[c.index(1)])**2
+    elif max(c) == 2:
+        if min(c) == 1:
+            return eval('*'.join([str(l[i]) for i, x in enumerate(c) if x == 1]))
+        else:
+            return (max(l) + min(l)) * abs(max(l) - min(l))
+    else:
+        return min(l)
