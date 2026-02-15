628b73ac09450049de750c8fa818baaadb666f32
Stella <107995643+DA-StellaPark@users.noreply.github.com>
2026-02-15 22:20:03 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.05 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -1,16 +1,3 @@
 def solution(lines):
-    visit:list = [False for _ in range(200)]
-    L:list = [[lines[i][0] + 100, lines[i][1] + 100] for i in range(3)]
-    for i in range(3) :
-        TEMP:list = [False for _ in range(200)]
-        for j in range(L[i][0], L[i][1], 1) :
-            TEMP[j] = True
-        for j in range(L[(i+1) % 3][0], L[(i+1) % 3][1], 1) :
-            if TEMP[j] :
-                visit[j] = True
-    answer = 0
-    for i in range(200) :
-        if visit[i] :
-            answer += 1
-
-    return answer
+    sets = [set(range(min(l), max(l))) for l in lines]
+    return len(sets[0] & sets[1] | sets[0] & sets[2] | sets[1] & sets[2])
