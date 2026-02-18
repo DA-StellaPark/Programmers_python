24f2b16a1b4d58a21ff808ea16dd9bee61672bd7
Stella <107995643+DA-StellaPark@users.noreply.github.com>
2026-02-18 23:59:50 +0900

    [level 1] Title: 소수 만들기, Time: 183.50 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,18 +1,11 @@
 def solution(nums):
-    n = len(nums)
+    from itertools import combinations as cb
     answer = 0
-    
-    for i in range(n-2):
-        for j in range(i+1, n-1):
-            for k in range(j+1, n):
-                total = nums[i] + nums[j] + nums[k]
-                
-                count = 0
-                for s in range(1, total+1):
-                    if total % s == 0:
-                        count += 1
-                        
-                if count == 2:   # 소수일 때만
-                    answer += 1
-        
+    for a in cb(nums, 3):
+        cand = sum(a)
+        for j in range(2, cand):
+            if cand%j==0:
+                break
+        else:
+            answer += 1
     return answer
