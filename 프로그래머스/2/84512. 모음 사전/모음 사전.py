848ad3bf2e3aad50ad61d6e7dffc6f006702e90c
Stella <107995643+DA-StellaPark@users.noreply.github.com>
2026-02-23 19:03:50 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,24 +1,5 @@
-import itertools
-
 def solution(word):
     answer = 0
-    letter = ['A', 'E', 'I', 'O','U']
-    combi = []
-    for i in range(1, 6):
-        combi += list(itertools.combinations_with_replacement((letter), i))
-
-    combi.sort()
-    perm = []
-    for c in combi:
-        perm += list(set(list(itertools.permutations(c))))
-
-    w = tuple(word)
-    perm.sort()
-
-    print(len(perm))
-    return perm.index(w) +1
-
-
-
-
-
+    for i, n in enumerate(word):
+        answer += (5 ** (5 - i) - 1) / (5 - 1) * "AEIOU".index(n) + 1
+    return answer
