50cf182c360cd01fd124c55ddb4786179824bf4c
Stella <107995643+DA-StellaPark@users.noreply.github.com>
2026-04-07 23:55:17 +0900

    [level 1] Title: 모의고사, Time: 4.99 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,33 +1,20 @@
 def solution(answers):
-    answer = []
-    first = [1, 2, 3, 4, 5] * 2000
-    second = [2, 1, 2, 3, 2, 4, 2, 5] * 1250
-    third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] * 1000
-    ans_1 = 0
-    ans_2 = 0
-    ans_3 = 0
-    for i in range(len(answers)):
-        if first[i] == answers[i]:
-            ans_1 += 1
+    pattern1 = [1,2,3,4,5]
+    pattern2 = [2,1,2,3,2,4,2,5]
+    pattern3 = [3,3,1,1,2,2,4,4,5,5]
+    score = [0, 0, 0]
+    result = []
 
-        if second[i] == answers[i]:
-            ans_2 += 1
+    for idx, answer in enumerate(answers):
+        if answer == pattern1[idx%len(pattern1)]:
+            score[0] += 1
+        if answer == pattern2[idx%len(pattern2)]:
+            score[1] += 1
+        if answer == pattern3[idx%len(pattern3)]:
+            score[2] += 1
 
-        if third[i] == answers[i]:
-            ans_3 += 1
+    for idx, s in enumerate(score):
+        if s == max(score):
+            result.append(idx+1)
 
-    if ans_1 > ans_2 and ans_1 > ans_3:
-        answer.append(1)
-    elif ans_2 > ans_1 and ans_2 > ans_3:
-        answer.append(2)
-    elif ans_3 > ans_1 and ans_3 > ans_2:
-        answer.append(3)
-    elif ans_1 == ans_2 and ans_1 > ans_3:
-        answer = [1, 2]
-    elif ans_1 == ans_3 and ans_1 > ans_2:
-        answer = [1, 3]
-    elif ans_2 == ans_3 and ans_2 > ans_1:
-        answer = [2, 3]
-    else:
-        answer = [1, 2, 3]
-    return answer
+    return result
