e60205d696620804de2b2f72cd1825fd77ca8717
Stella <107995643+DA-StellaPark@users.noreply.github.com>
2026-04-29 01:14:40 +0900

    [level 0] Title: 합성수 찾기, Time: 0.07 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,6 +1,6 @@
 def solution(n):
     output = 0
-    for i in range(4, n + 1):
+    for i in range(4, n + 1): # 3 이하는 소수, 4부터
         for j in range(2, int(i ** 0.5) + 1):
             if i % j == 0:
                 output += 1
