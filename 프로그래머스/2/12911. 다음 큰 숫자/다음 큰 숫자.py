0e83917419488f9005f8505883ad199abbcf6cab
Stella <107995643+DA-StellaPark@users.noreply.github.com>
2026-05-13 22:58:58 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,5 +1,4 @@
 def solution(n):
-    
     #자연수 n을 이진수로 변환했을 때 1의 갯수
     cnt1 = bin(n)[2:].count("1")
     
@@ -8,5 +7,4 @@ def solution(n):
         n += 1
         if cnt1 == bin(n)[2:].count("1"):
             break
-
     return n
