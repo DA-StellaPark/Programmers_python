56fa42cd59cb5c6c3938bcdfb4e64e078085426f
Stella <107995643+DA-StellaPark@users.noreply.github.com>
2026-05-16 23:58:32 +0900

    [level 2] Title: 호텔 대실, Time: 13.33 ms, Memory: 9.57 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -1,12 +1,13 @@
-def to_minutes(time):
+def to_minutes(time): # 분으로 통일
     h, m = map(int, time.split(':'))
     return h * 60 + m 
 
 def solution(book_time):
     rooms = []
-
-    books = sorted([(to_minutes(start), to_minutes(end) + 10) for start, end in book_time])
-
+    books = sorted([
+        (to_minutes(start), to_minutes(end) + 10) # 청소시간까지 추가
+        for start, end in book_time
+        ])
 
     for start, end in books:
         for i in range(len(rooms)):
@@ -15,7 +16,7 @@ def solution(book_time):
                 break
         else:
             rooms.append(end)
-
+            
         rooms.sort()
 
     return len(rooms)
