2e2d30be02fca3def6231686cf88f2726eaee503
Stella <107995643+DA-StellaPark@users.noreply.github.com>
2026-06-03 23:59:27 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 0.05 ms, Memory: 7.75 MB -BaekjoonHub

---
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.py
@@ -0,0 +1,11 @@
+# x씩 증가하는 숫자 n개
+# 1
+def solution(x, n):
+    answer = []
+    for i in range(1, n+1):
+        answer.append(x*i)
+    return answer
+
+# 2
+# def solution(x,n):
+#     return [x*i for i in range(1,n+1)] 
